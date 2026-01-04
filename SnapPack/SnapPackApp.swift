8aee304c0cd299f26087b1d8f985768aa2d419f3
AfterMath <alam24ir@student.ju.se>
2026-01-04 08:24:28 +0100

    Initial Commit

---
+++ SnapPack/SnapPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapPackApp.swift
+//  SnapPack
+//
+//  Created by Amr Mafalani on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct SnapPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
